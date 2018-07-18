9b49ad1cafe1052ac7a4dca9f3cc05107533f3c6
Calvin Jeong <calvin@ty.pe.kr>
2018-07-18 21:41:51 +1000

    Update urls.py

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .views import SecureView, UnsecureView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^/page/secure/$', SecureView.as_view(), {'SSL': True}, name='secure_page'),
     url(r'^/page/unsecure/$', UnsecureView.as_view(), {'SSL': False}, name='unsecure_page'),
     url(r'^/page/default/$', UnsecureView.as_view(), name='default_page'),
-)
+]
